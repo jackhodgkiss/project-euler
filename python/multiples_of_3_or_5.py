55e48339c737e0c15ded167be21a9f63adb6e37e
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-07-16 21:22:13 +0100

    fix: apply `pep8` formatting

--- python/multiples_of_3_or_5.py
+++ python/multiples_of_3_or_5.py
@@ -3,7 +3,8 @@ from typing import Callable, Sequence
 
 
 def main() -> None:
-    mod_filter: Callable[[int], bool] = lambda number: number % 3 == 0 or number % 5 == 0
+    mod_filter: Callable[[int], bool] = \
+        lambda number: number % 3 == 0 or number % 5 == 0
     numbers: Sequence = range(1, 1_000)
     answer: int = sum(filter(mod_filter, numbers))
     print(f"Answer: {answer}")
