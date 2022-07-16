baf1b5cb9dd389990bec0ebfe38443dfa9ae53b2
Jack Hodgkiss <identity@jackhodgkiss.uk>
2022-07-16 21:05:05 +0100

    feat: add `multiples_of_3_or_5.py`

---
+++ python/multiples_of_3_or_5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import Callable, Sequence
+
+
+def main() -> None:
+    mod_filter: Callable[[int], bool] = lambda number: number % 3 == 0 or number % 5 == 0
+    numbers: Sequence = range(1, 1_000)
+    answer: int = sum(filter(mod_filter, numbers))
+    print(f"Answer: {answer}")
+
+
+if __name__ == "__main__":
+    main()
